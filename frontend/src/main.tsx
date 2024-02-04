023373615d04b8d875e18803a74d54543ccd6fd6
Adonis Lee Villamor <adonisv79@gmail.com>
2024-02-04 18:05:08 +0800

    tailwind [https://tailwindcss.com/docs/guides/vite]

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+import "./styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LoginPage } from "./pages/login.tsx";
 import { i18n } from "./i18n";
 import { HomePage } from "./pages/homepage.tsx";
 const queryClient = new QueryClient();
-const userLanguage = localStorage.getItem('lang') || 'en';
+const userLanguage = localStorage.getItem("lang") || "en";
 
 const router = createBrowserRouter([
   {
